3409ea2b9af5fd1b45caa59a4f9d414d50c55b21
will <64425242+willser@users.noreply.github.com>
2020-07-05 11:42:14 +0800

    refactor: change project api inter

---
+++ src/main/java/com/ultlog/common/model/Project.java
@@ -0,0 +1,26 @@
+package com.ultlog.common.model;
+
+/**
+ * @program: common
+ * @link: github.com/ultlog/common
+ * @author: will
+ * @create: 2020-06-28
+ **/
+public class Project {
+
+    /**
+     * name of project
+     */
+    private String project;
+
+    /**
+     * name of module
+     */
+    private String module;
+
+    /**
+     * uuid of instance(container)
+     */
+    private String uuid;
+
+}
